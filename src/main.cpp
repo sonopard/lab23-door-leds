d85f63e6a721cc3acaf67aecfebafaed32e3f3f3
Benjamin Kunz <benjamin.kunz@uni-wuerzburg.de>
2021-08-30 12:54:15 +0200

    current test version

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <FS.h> 
 
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 #include <DNSServer.h>
-#include <ESPAsyncWiFiManager.h>     
+#include <ArduinoOTA.h>
 #include <NeoPixelBus.h>
-#include <ArduinoJson.h>
+#include <WiFiManager.h>
+#include "animations.h"
 
 #define HOSTNAME_PREFIX "door-leds-1"
 
 #define NUM_LEDS (134+134+134+134)
-#define SATURATION 16
+#define SATURATION 32
 
 //      ====|====
 //      ====|====
@@ -21,12 +20,13 @@
 //      |AUSGANG|
 //      |=======|
 // 
-// L_L   ^ -> R_L  ????? not sure anymore tbh
-//  |    |     |
-//  |    |     |
-//  |    |     |
-//  \ > L_R    v
+// L_L   ^ -> R_L   NUM_LEDS
+//  |    |     |    |
+//  v    ^     v    ^
+//  |    |     |    |
+//  \ > L_R    \ > R_R
 //
+
 #define LED_INDEX_SEG_L_L 0
 #define LED_INDEX_SEG_L_R (LED_INDEX_SEG_L_L + 134)
 #define LED_INDEX_SEG_R_L (LED_INDEX_SEG_L_R + 134)
@@ -54,32 +54,19 @@ HslColor hslBlue(blue);
 HslColor hslWhite(white);
 HslColor hslBlack(black);
 
-
 WiFiUDP cmd_udp;
 uint8_t pkt_data[PKT_MAX_LEN];
 char resp_data[2];
 char cb[200];
 
-//flag for saving data
-bool shouldSaveConfig = false;
-
-//callback notifying us of the need to save config
-void saveConfigCallback () {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
-}
-
-AsyncWebServer server(80);
-DNSServer dns;
-
 void setup()
 {
-    Serial.begin(115200);
-    while (!Serial); // wait for serial attach
+  Serial.begin(115200);
+  while (!Serial); // wait for serial attach
 
-    Serial.println();
-    Serial.println("Initializing...");
-    Serial.flush();
+  Serial.println();
+  Serial.println("Initializing...");
+  Serial.flush();
   // construct hostname string
   String hostname = String(strlen(HOSTNAME_PREFIX) + 6 + 1);
   hostname = HOSTNAME_PREFIX;
@@ -94,11 +81,7 @@ void setup()
   
   WiFi.hostname(hostname);
   
-  AsyncWiFiManager wifiManager(&server,&dns);
-  
-  //set config save notify callback
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-
+  WiFiManager wifiManager;
   // uncomment to reset saved settings
   // wifiManager.resetSettings();
 
@@ -107,25 +90,51 @@ void setup()
   // and goes into a blocking loop awaiting configuration
   wifiManager.autoConnect(cb);
   Serial.println("connected...yeey :)");
-  
+
   // print this client's IP on the UART for debugging purposes
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
   cmd_udp.begin(UDP_PORT);
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "I am door-leds-1.");
+  ArduinoOTA.setHostname(cb);
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
   });
 
-  AsyncElegantOTA.begin(&server);
-  server.begin();
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 
-    // this resets all the neopixels to an off state
-    strip.Begin();
-    strip.Show();
-
-
+  // this resets all the neopixels to an off state
+  strip.Begin();
+  strip.Show();
 }
 
 void strobe_fn(uint8_t *fps) {
@@ -138,6 +147,9 @@ void strobe_fn(uint8_t *fps) {
       strip.SetPixelColor(p, color);
     }
     for (int p = LED_INDEX_SEG_L_R; p < LED_INDEX_SEG_R_R; p++) {
+      strip.SetPixelColor(p, hslBlack);
+    }
+        for (int p = LED_INDEX_SEG_L_R; p < LED_INDEX_SEG_R_R; p++) {
       strip.SetPixelColor(p, hslBlack);
     }
   } else { 
@@ -213,19 +225,22 @@ void net_parse() {
 
 void loop()
 {
-    static unsigned long loop_ms = 0;
-    
-    net_parse();
-    digitalWrite(LED_BUILTIN, 0);
-        
-    if(anim_fn) {
-      if((millis() - loop_ms) > (1000/fps)) {
-        anim_fn(&fps);
-        strip.Show();
-        if(anim_fn == off_fn) 
-          anim_fn = NULL;
-        loop_ms = millis();
-      }
+  static unsigned long loop_ms = 0;
+  
+//  AsyncElegantOTA.loop();
+  ArduinoOTA.handle();
+
+  net_parse();
+  digitalWrite(LED_BUILTIN, 0);
+      
+  if(anim_fn) {
+    if((millis() - loop_ms) > (1000/fps)) {
+      anim_fn(&fps);
+      strip.Show();
+      if(anim_fn == off_fn) 
+        anim_fn = NULL;
+      loop_ms = millis();
     }
+  }
 }
 
